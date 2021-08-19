4fb31866fbd78afeab40b41a9d6b0847ddfe8f01
Kirill <kirillryabushev@gmail.com>
2021-08-19 18:29:37 +1100

    add events

---
+++ js/catalog_accordion.js
@@ -0,0 +1,9 @@
+$( function() {
+  $( ".catalog__accordion" ).accordion({
+    collapsible: true,
+    heightStyle: "content",
+    active: 0,
+    animate: 400,
+    icons: false,
+  });
+});
---
+++ js/events_swiper.js
@@ -0,0 +1,9 @@
+var swiper2 = new Swiper('.events__cards-swiper', {
+  allowTouchMove: false,
+  slidesPerView: 'auto',
+  slidesPerGroup: 3,
+  pagination: {
+    el: '.events__swiper-pagination',
+    type: 'bullets',
+  },
+});
