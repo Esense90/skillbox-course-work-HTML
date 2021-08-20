cbf3ddbf1e4923c77104123cb0e69f30f652d297
Kirill <kirillryabushev@gmail.com>
2021-08-20 13:39:33 +1100

    add price

--- js/events_swiper.js
+++ js/events_swiper.js
@@ -1,9 +1,13 @@
-var swiper2 = new Swiper('.events__cards-swiper', {
-  allowTouchMove: false,
-  slidesPerView: 'auto',
-  slidesPerGroup: 3,
-  pagination: {
-    el: '.events__swiper-pagination',
-    type: 'bullets',
-  },
-});
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('.events__footer-button').forEach(function (footerButton) {
+    footerButton.addEventListener('click', function (event) {
+      document.querySelectorAll('.events__cards-item').forEach(function (cardsItem) {
+        cardsItem.classList.remove('events__cards-hidden')
+      })
+      document.querySelectorAll('.events__footer-button').forEach(function (cardsItem) {
+        cardsItem.classList.add('events__cards-hidden')
+      })
+    })
+  })
+})
+
