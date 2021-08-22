c8a6cdf67e301149364e47aaf6db21acfc833d9b
Kirill <kirillryabushev@gmail.com>
2021-08-23 10:30:23 +1100

    cancel 1920 dekstop

---
+++ js/map.js
@@ -0,0 +1,15 @@
+ymaps.ready(init);
+    function init() {
+        var myMap = new ymaps.Map("map", {
+          center: [55.766152, 37.634703],
+          zoom: 14
+    });
+        
+    var myPlacemark = new ymaps.Placemark([55.757176, 37.602085], {}, {
+          iconLayout:'default#image',
+          iconImageHref: './img/marker.svg',    
+          iconImageSize: [20, 20],
+          
+        });
+          myMap.geoObjects.add(myPlacemark);
+}
