0165e4dadcd79750149484128082c1b5dbe30123
Kirill <kirillryabushev@gmail.com>
2021-08-23 18:41:09 +1100

    start med

--- js/gallery_slider.js
+++ js/gallery_slider.js
@@ -1,33 +1,9 @@
 var swiper1 = new Swiper('.gallery__swiper-container', {
-  slidesPerView: 1,
   slidesPerColumnFill: 'row',
-  breakpoints: {
-    576: {
-      slidesPerView: 2,
-      slidesPerColumn: 2,
-      spaceBetween: 34,
-      slidesPerGroup: 2,
-    },
-    768: {
-      slidesPerView: 2,
-      slidesPerColumn: 2,
-      spaceBetween: 34,
-      slidesPerGroup: 2,
-    },
-    1024: {
-      slidesPerView: 2,
-      slidesPerColumn: 2,
-      spaceBetween: 35,
-      slidesPerGroup: 2,
-    },
-    1368: {
-      slidesPerView: 3,
-      slidesPerColumn: 2,
-      spaceBetween: 50,
-      slidesPerGroup: 3,
-    },
-  },
-
+  slidesPerView: 3,
+  slidesPerColumn: 2,
+  slidesPerGroup: 3,
+  spaceBetween: 50,
   navigation: {
     nextEl: '.gallery__button-next',
     prevEl: '.gallery__button-prev',
