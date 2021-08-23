2ae81a01f9ea71ac9466f5292abbffea88926e03
Kirill <kirillryabushev@gmail.com>
2021-08-23 17:30:02 +1100

    start media

--- js/gallery_slider.js
+++ js/gallery_slider.js
@@ -1,10 +1,33 @@
 var swiper1 = new Swiper('.gallery__swiper-container', {
-  allowTouchMove: false,
+  slidesPerView: 1,
   slidesPerColumnFill: 'row',
-  slidesPerColumn: 2,
-  slidesPerView: 'auto',
-  slidesPerGroup: 3,
-  spaceBetween: 50,
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+      slidesPerColumn: 2,
+      spaceBetween: 34,
+      slidesPerGroup: 2,
+    },
+    768: {
+      slidesPerView: 2,
+      slidesPerColumn: 2,
+      spaceBetween: 34,
+      slidesPerGroup: 2,
+    },
+    1024: {
+      slidesPerView: 2,
+      slidesPerColumn: 2,
+      spaceBetween: 35,
+      slidesPerGroup: 2,
+    },
+    1368: {
+      slidesPerView: 3,
+      slidesPerColumn: 2,
+      spaceBetween: 50,
+      slidesPerGroup: 3,
+    },
+  },
+
   navigation: {
     nextEl: '.gallery__button-next',
     prevEl: '.gallery__button-prev',
@@ -14,3 +37,6 @@ var swiper1 = new Swiper('.gallery__swiper-container', {
     type: 'fraction',
   },
 });
+
+
+
