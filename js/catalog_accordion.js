9f746837e72c9417ddf7e3e2ce1a15693d728ad4
Kirill <kirillryabushev@gmail.com>
2021-10-02 22:37:42 +1100

    changes1

--- js/catalog_accordion.js
+++ js/catalog_accordion.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function() {
   $(".catalog__accordion").accordion({
     collapsible: true,
     heightStyle: "content",
@@ -6,25 +6,26 @@ $(function () {
     animate: 400,
     icons: false,
 
-    activate: function(event,ui) {
+    activate: function(event, ui) {
       console.log(ui);
       if (ui.oldHeader[0]) {
         ui.oldHeader[0].closest('.catalog__accordion-item').
-          classList.remove('catalog__accordion_active')
+        classList.remove('catalog__accordion_active')
       }
       if (ui.newHeader[0]) {
         ui.newHeader[0].closest('.catalog__accordion-item').
-          classList.add('catalog__accordion_active')
+        classList.add('catalog__accordion_active')
       }
     }
   });
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelectorAll('.accordion__content-btn').forEach(function (catalofCountriesItem) {
-    catalofCountriesItem.addEventListener('click', function (event) {
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('.accordion__content-btn').forEach(function(catalofCountriesItem) {
+    catalofCountriesItem.addEventListener('click', function(event) {
       const path = event.currentTarget.dataset.path
-      document.querySelectorAll('.accordion__content-btn').forEach(function (catalogCountriesDiscriptionCard) {
+
+      document.querySelectorAll('.accordion__content-btn').forEach(function(catalogCountriesDiscriptionCard) {
         catalogCountriesDiscriptionCard.classList.remove('accordion__artist-active')
       })
       document.querySelector(`[data-target="${path}"]`).classList.add('accordion__artist-active')
