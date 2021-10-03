4afd775a8a2d27e365a6d048336c2c06d7311ad2
Kirill <kirillryabushev@gmail.com>
2021-10-03 17:10:04 +1100

    add changes

--- js/gallery_slider.js
+++ js/gallery_slider.js
@@ -30,8 +30,17 @@ var swiper1 = new Swiper('.gallery__swiper-container', {
     nextEl: '.gallery__button-next',
     prevEl: '.gallery__button-prev',
   },
+  observer: true,
+  observeParents: true,
   pagination: {
     el: '.gallery__swiper-pagination',
     type: 'fraction',
   },
+  on: {
+    beforeResize: function() {
+      this.slides.forEach((el) => {
+        el.style.marginTop = "";
+      });
+    }
+  }
 });
--- js/map.js
+++ js/map.js
@@ -3,19 +3,13 @@ ymaps.ready(init);
 function init() {
   var myMap = new ymaps.Map("map", {
     center: [55.758965, 37.602840],
-    zoom: 14
+    zoom: 14,
   });
 
   var myPlacemark = new ymaps.Placemark([55.757176, 37.602085], {}, {
     iconLayout: 'default#image',
     iconImageHref: './img/marker.svg',
     iconImageSize: [20, 20],
-
   });
   myMap.geoObjects.add(myPlacemark);
 }
-window.addEventListener('resize', () => {
-  eventHidden();
-})
-
-eventHidden();
