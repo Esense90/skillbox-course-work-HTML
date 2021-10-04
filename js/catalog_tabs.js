ab109fdf073e551d54ed0797cd359652172e9c1e
Kirill <kirillryabushev@gmail.com>
2021-10-04 19:29:04 +1100

    cancel

--- js/catalog_tabs.js
+++ js/catalog_tabs.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelectorAll('.catalog__country-item').forEach(function (catalofCountriesItem) {
-    catalofCountriesItem.addEventListener('click', function (event) {
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('.catalog__country-item').forEach(function(catalofCountriesItem) {
+    catalofCountriesItem.addEventListener('click', function(event) {
       const path = event.currentTarget.dataset.path
-      document.querySelectorAll('.catalog__countries-discription_content').forEach(function (catalogCountriesDiscriptionCard) {
+      document.querySelectorAll('.catalog__countries-discription_content').forEach(function(catalogCountriesDiscriptionCard) {
         catalogCountriesDiscriptionCard.classList.remove('catalog__active')
       })
       document.querySelector(`[data-target="${path}"]`).classList.add('catalog__active')
@@ -11,14 +11,27 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelectorAll('.catalog__country-btn').forEach(function (catalogCountryItem) {
-    catalogCountryItem.addEventListener('click', function (event) {
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('.catalog__country-btn').forEach(function(catalogCountryItem) {
+    catalogCountryItem.addEventListener('click', function(event) {
       const path = event.currentTarget.dataset.path
-      document.querySelectorAll('.catalog__country-btn').forEach(function (catalogCountryItem) {
+      document.querySelectorAll('.catalog__country-btn').forEach(function(catalogCountryItem) {
         catalogCountryItem.classList.remove('country__btn-active')
       })
       event.currentTarget.classList.add('country__btn-active')
+    })
+  })
+})
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('.catalog__accordion-content_item').forEach(function(catalofCountriesItem) {
+    catalofCountriesItem.addEventListener('click', function(event) {
+      const btn = event.currentTarget.dataset.btn
+      document.querySelectorAll('.catalog__countries-discription_card').forEach(function(catalogCountriesDiscriptionCard) {
+        catalogCountriesDiscriptionCard.classList.remove('painter__active')
+      })
+      document.querySelector(`[data-view="${btn}"]`).classList.add('painter__active')
     })
   })
 })
