ce9d7521be10576868dd83b10189501fd64f7d29
Kirill <kirillryabushev@gmail.com>
2021-10-15 20:16:57 +1100

    cancel diplom1

--- js/send.js
+++ js/send.js
@@ -36,7 +36,7 @@ let validateForms = function(selector, rules, successModal, yaGoal) {
             }, 100);
             setTimeout(function() {
               document.querySelector('.thanks__popup').classList.remove('thanks__popup-active');
-            }, 5000);
+            }, 3000);
           }
         }
       }
